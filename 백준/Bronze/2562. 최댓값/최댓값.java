0964a4616737d85e49e51cccf383022fcd693571
ksj <132119447+KSJ0314@users.noreply.github.com>
2023-06-28 14:11:25 +0900

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+
+		int[] num = new int[9];
+
+		for (int i = 0; i < 9; i++) {
+			num[i] = s.nextInt();
+		}
+		int max = num[0];
+		int maxIndex = 1;
+
+		for (int i = 1; i < 9; i++) {
+			if (max < num[i]) {
+				max = num[i];
+				maxIndex = i + 1;
+			}
+		}
+		System.out.println(max);
+		System.out.println(maxIndex);
+
+	}
+
+}

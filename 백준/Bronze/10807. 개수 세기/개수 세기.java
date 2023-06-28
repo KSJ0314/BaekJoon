e3eb7d4f1d55c7f74036ac212a0353ef59ca0188
ksj <132119447+KSJ0314@users.noreply.github.com>
2023-06-28 11:25:00 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,25 +1,41 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner s = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N = s.nextInt();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
 
 		int[] num = new int[N];
+
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			num[i] = s.nextInt();
+			num[i] = Integer.parseInt(st.nextToken());
 		}
-		int v = s.nextInt();
+
+		int v = Integer.parseInt(br.readLine());
+
 		int sum = 0;
+
 		for (int i = 0; i < N; i++) {
 			if (num[i] == v) {
 				sum++;
 			}
 		}
 		System.out.println(sum);
+
+		bw.close();
 
 	}
 

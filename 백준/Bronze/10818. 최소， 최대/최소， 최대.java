03976a9f85fa020400d32066b40bf62025f17b2e
ksj <132119447+KSJ0314@users.noreply.github.com>
2023-06-28 12:53:08 +0900

    [Bronze III] Title: 최소, 최대, Time: 508 ms, Memory: 91904 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[] num = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int min = num[0];
+		int max = num[0];
+
+		for (int i = 1; i < N; i++) {
+			if (min > num[i]) {
+				min = num[i];
+			}
+			if (max < num[i]) {
+				max = num[i];
+			}
+		}
+
+		System.out.printf("%d %d", min, max);
+
+	}
+
+}

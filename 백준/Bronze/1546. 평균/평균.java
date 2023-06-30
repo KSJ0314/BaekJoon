b3111b338ba126213b0d510e7bcbb88b43a56bd1
ksj <132119447+KSJ0314@users.noreply.github.com>
2023-06-30 16:32:37 +0900

    [Bronze I] Title: 평균, Time: 132 ms, Memory: 14528 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+
+		double[] arrGrade = new double[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arrGrade[i] = Double.parseDouble(st.nextToken());
+		}
+
+		double max = arrGrade[0];
+		for (int i = 1; i < N; i++) {
+			if (max < arrGrade[i]) {
+				max = arrGrade[i];
+			}
+		}
+		double sum = 0;
+		for (int i = 0; i < N; i++) {
+			arrGrade[i] = arrGrade[i] / max * 100;
+			sum += arrGrade[i];
+		}
+		double avg = sum / N;
+		System.out.println(avg);
+
+	}
+
+}

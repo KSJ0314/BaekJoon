795abecb15c59604b7ce9c63f62faad904ace310
ksj <132119447+KSJ0314@users.noreply.github.com>
2023-09-13 20:00:45 +0900

    [Bronze II] Title: 벌집, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		N--;
+		int i = 1;
+		while (N>0) {
+			N-=i*6;
+			i++;
+		}
+		System.out.println(i);
+		
+	}
+
+}

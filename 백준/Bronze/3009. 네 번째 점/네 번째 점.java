1c0d04c98e7d653969112a1722e827f46ba26ad8
ksj <132119447+KSJ0314@users.noreply.github.com>
2024-07-01 16:52:17 +0900

    [Bronze III] Title: 네 번째 점, Time: 192 ms, Memory: 18388 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.java
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -5,29 +5,24 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int[][] coor = new int[3][3];
+		int[][] coors = new int[3][3];
 		
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 2; j++) {
-				coor[i][j] = sc.nextInt();
+				coors[i][j] = sc.nextInt();
 			}
 		}
 		
-		int x = coor[0][0];
-		if (x == coor[1][0]) {
-			x = coor[2][0];
-		} else if (x == coor[2][0]) {
-			x = coor[1][0];
+		int[] coor = new int[2];
+		for (int i = 0; i < 2; i++) {
+			coor[i] = coors[0][i];
+			if (coor[i] == coors[1][i]) {
+				coor[i] = coors[2][i];
+			} else if (coor[i] == coors[2][i]) {
+				coor[i] = coors[1][i];
+			}
 		}
-		
-		int y = coor[0][1];
-		if (y == coor[1][1]) {
-			y = coor[2][1];
-		} else if (y == coor[2][1]) {
-			y = coor[1][1];
-		}
-		
-		System.out.println(x + " " + y);
+		System.out.println(coor[0] + " " + coor[1]);
 		
 	}
 

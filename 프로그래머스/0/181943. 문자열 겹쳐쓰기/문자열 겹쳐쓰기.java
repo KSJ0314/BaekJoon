da135e3141ca99d152c7d4e00fc472e758a37b13
ksj <132119447+KSJ0314@users.noreply.github.com>
2024-11-17 00:46:34 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 2.78 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,13 +1,9 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         String answer = "";
-        char[] ch = my_string.toCharArray();
-        char[] ch2 = overwrite_string.toCharArray();
-        
-        for (int i = 0; i < overwrite_string.length(); i++) {
-			ch[i+s] = ch2[i];
-		}
-        answer = new String(ch);
+        answer += my_string.substring(0, s);
+        answer += overwrite_string;
+        answer += my_string.substring(overwrite_string.length()+s);
         return answer;
     }
 }

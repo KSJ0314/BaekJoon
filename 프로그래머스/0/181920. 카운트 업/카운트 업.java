62857f823827957c6258c70c7f84e30c67942603
ksj <132119447+KSJ0314@users.noreply.github.com>
2024-11-25 17:15:56 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,11 +1,9 @@
-import java.util.ArrayList;
-
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        ArrayList<Integer> list = new ArrayList<>();
-		for (int i = start_num; i <= end_num; i++) {
-			list.add(i);
+        int[] answer = new int[end_num - start_num + 1];
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = start_num+i;
 		}
-        return list.stream().mapToInt(i -> i).toArray();
+        return answer;
     }
 }

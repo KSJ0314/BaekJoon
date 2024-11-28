32b5990cb3e53f8484326bceafc838a1cb5a4d2f
ksj <132119447+KSJ0314@users.noreply.github.com>
2024-11-28 19:31:31 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 2.77 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,11 +1,9 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] num_list) {
-        int mul = 1;
-        int sum = 0;
-        for (int num : num_list){
-            sum += num;
-            mul *= num;
-        }
-        return mul <= Math.pow(sum, 2) ? 1 : 0;
+        int mul = Arrays.stream(num_list).reduce(1, (a,b) -> a*b);
+        double sumPow = Math.pow(Arrays.stream(num_list).sum(), 2);
+        return mul <= sumPow ? 1 : 0;
     }
 }

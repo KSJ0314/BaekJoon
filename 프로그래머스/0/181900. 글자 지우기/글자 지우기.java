1a13d498539c4280bb926e0ec4211d0f8dfbbd62
ksj <132119447+KSJ0314@users.noreply.github.com>
2024-11-28 18:38:29 +0900

    [level 0] Title: 글자 지우기, Time: 0.43 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,18 +1,11 @@
-import java.util.*;
-
 class Solution {
     public String solution(String my_string, int[] indices) {
-        StringBuilder sb = new StringBuilder();
+        String[] strArr = my_string.split("");
         
-        for (int i = 0; i < my_string.length(); i++) {
-        	int currentIndex = i;
-        	boolean isContain = Arrays.stream(indices)
-        							.anyMatch(num -> num == currentIndex);
-        	if (!isContain) {
-        		sb.append(my_string.charAt(i));
-        	}
+        for (int indice : indices) {
+        	strArr[indice] = "";
         }
         
-        return sb.toString();
+        return String.join("", strArr);
     }
 }

6f9a91f4c2db298f8da35a181a90e5af9b117720
ksj <132119447+KSJ0314@users.noreply.github.com>
2024-12-01 19:25:02 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.70 ms, Memory: 89.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int[] querie : queries){
+            for (int i = querie[0]; i <= querie[1]; i++){
+                arr[i]++;
+            }
+        }
+        return arr;
+    }
+}

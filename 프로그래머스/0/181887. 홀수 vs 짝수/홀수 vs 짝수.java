1ab1dea115559ae7b2b22c51528e2d183610d93c
ksj <132119447+KSJ0314@users.noreply.github.com>
2024-12-01 18:54:25 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 6.02 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -2,12 +2,10 @@ import java.util.stream.IntStream;
 
 class Solution {
 	public int solution(int[] num_list) {
-        int even = IntStream.range(0, num_list.length)
-		        	.filter(i -> i % 2 == 0)
+        int even = IntStream.iterate(0, i -> i < num_list.length, i -> i+2)
 		        	.map(i -> num_list[i])
 		        	.sum();
-        int odd = IntStream.range(0, num_list.length)
-	        		.filter(i -> i % 2 == 1)
+        int odd = IntStream.iterate(1, i -> i < num_list.length, i -> i+2)
 	        		.map(i -> num_list[i])
 	        		.sum();
         return Math.max(even, odd);

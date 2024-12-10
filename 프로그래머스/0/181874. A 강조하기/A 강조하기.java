9d79d8f569e8df187a742f140c5894273ea9d61b
ksj <132119447+KSJ0314@users.noreply.github.com>
2024-12-10 19:16:32 +0900

    [level 0] Title: A 강조하기, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,9 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        StringBuilder sb = new StringBuilder();
-        for (char ch : myString.toUpperCase().toCharArray()){
-            sb.append(ch == 'A' ? ch : Character.toLowerCase(ch));
-        }
-        return sb.toString();
+        return myString.toLowerCase().replace('a', 'A');
     }
 }

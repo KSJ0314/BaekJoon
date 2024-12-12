499e1f1e15dc78ac8f2abfe53ac909fcfb67f838
ksj <132119447+KSJ0314@users.noreply.github.com>
2024-12-12 18:56:40 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 1.05 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,11 +1,5 @@
 class Solution {
     public String[] solution(String my_string) {
-        while (my_string.startsWith(" ")) {
-			my_string = my_string.substring(1);
-		}
-		while (my_string.contains("  ")) {
-			my_string = my_string.replace("  ", " ");
-		}
-        return my_string.split(" ");
+        return my_string.trim().split("\\s+");
     }
 }

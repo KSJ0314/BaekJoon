5c74f47abdbe986794e14e20509454aad0f42fb5
ksj <132119447+KSJ0314@users.noreply.github.com>
2024-12-13 17:20:37 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,9 +1,8 @@
 class Solution {
     public int solution(String myString, String pat) {
-        StringBuilder sb = new StringBuilder();
-		for (char myChar : myString.toCharArray()) {
-			sb.append((char)(myChar^3));
-		}
-        return sb.toString().contains(pat) ? 1 : 0;
+        return myString.replace("A", "0")
+	        		.replace("B", "A")
+	        		.replace("0", "B")
+	        		.contains(pat) ? 1 : 0;
     }
 }

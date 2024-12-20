1e92fe94f82e6b497de49db2ea17a3b2cc5eaa66
ksj <132119447+KSJ0314@users.noreply.github.com>
2024-12-20 23:03:42 +0900

    [level 0] Title: 그림 확대, Time: 3.92 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -1,15 +1,16 @@
 class Solution {
     public String[] solution(String[] picture, int k) {
         String[] answer = new String[picture.length * k];
+        StringBuilder sb = new StringBuilder();
         
         for (int i = 0; i < answer.length; i++) {
-        	StringBuilder newPicture = new StringBuilder();
         	for (char ch : picture[i/k].toCharArray()) {
         		for (int j = 0; j < k; j++) {
-        			newPicture.append(ch);
+        			sb.append(ch);
 				}
         	}
-			answer[i] = newPicture.toString();
+			answer[i] = sb.toString();
+            sb.setLength(0);
 		}
         
         return answer;

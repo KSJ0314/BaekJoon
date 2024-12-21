88595643c18f28114c1c73421965b1ce8afc3a37
ksj <132119447+KSJ0314@users.noreply.github.com>
2024-12-21 16:02:33 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.16 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -2,19 +2,12 @@ import java.util.*;
 
 class Solution {
     public int[][] solution(int[][] arr) {
-        int col = arr.length;
-        int row = arr[0].length;
-        int max = Math.max(col, row);
-        
-        int[][] answer = Arrays.copyOf(arr, max);
-    	for (int i = col; i < max; i++) {
-			answer[i] = new int[max];
+        int max = Math.max(arr.length, arr[0].length);
+        int[][] answer = new int[max][max];
+                           
+		for (int i = 0; i < arr.length; i++) {
+			answer[i] = Arrays.copyOf(arr[i], max);
 		}
-    	if (row < max) {
-    		for (int i = 0; i < max; i++) {
-    			answer[i] = Arrays.copyOf(arr[i], max);
-    		}
-    	}
     	
         return answer;
     }

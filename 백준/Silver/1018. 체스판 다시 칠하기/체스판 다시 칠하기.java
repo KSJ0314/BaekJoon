3fa392c4b0f1e2c6a2da4d3c816998cb2613da17
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-01-05 18:23:03 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 116 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,27 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 	
-	public static int diffCounter(int[][]arr, int minDiff) {
+	public static int[][] arr;
+	public static int minDiff;
+	
+	public static void diffCounter(int y, int x) {
 		int diffCountB = 0;
 		int diffCountW = 0;
-		O: for (int i = 0; i < 8; i++) {	
-			for (int j = 0; j < 8; j++) {
-				if ((i+j)%2 == arr[i][j]) {	// 첫 칸 B
+		for (int i = y; i < y+8; i++) {	
+			for (int j = x; j < x+8; j++) {
+				if (((i-y)+(j-x))%2 == arr[i][j]) {	// 첫 칸 B
 					diffCountB++;
 				}
-				if ((i+j)%2 != arr[i][j]) {	// 첫 칸 W
+				if (((i-y)+(j-x))%2 != arr[i][j]) {	// 첫 칸 W
 					diffCountW++;
 				}
 				if (diffCountB > minDiff && diffCountW > minDiff) {
-					return minDiff;
+					return;
 				}
 			}
 		}
-		return Math.min(diffCountB, diffCountW);
+		minDiff = Math.min(diffCountB, diffCountW);
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -30,24 +32,20 @@ public class Main {
 		int N = Integer.parseInt(input[0]);
 		int M = Integer.parseInt(input[1]);
 		
-		// 입력 데이터를 ("B" -> true | "W" -> false)로 저장
-		int[][] arr = new int[N][M];
+		// 입력 데이터를 ("B" -> 1 | "W" -> 0)로 저장
+		arr = new int[N][M];
 		for (int i = 0; i < N; i++) {
 			int j = 0;
-			for (String temp : br.readLine().split("")) {
-				arr[i][j] = temp.equals("B") ? 1 : 0;
+			for (char temp : br.readLine().toCharArray()) {
+				arr[i][j] = temp=='B' ? 1 : 0;
 				j++;
 			}
 		}
 		
-		int[][] tempArr = new int[8][8];
-		int minDiff = 64;
+		minDiff = 64;
 		O: for (int i = 0; i <= N-8; i++) {
 			for (int j = 0; j <= M-8; j++) {
-				for (int k = i; k < i+8; k++) {
-					tempArr[k-i] = Arrays.copyOfRange(arr[k], j, j+8);
-				}
-				minDiff = diffCounter(tempArr, minDiff);
+				diffCounter(i, j);
 				if (minDiff == 0) {
 					break O;
 				}

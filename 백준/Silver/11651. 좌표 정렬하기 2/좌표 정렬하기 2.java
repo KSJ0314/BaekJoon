21868276c35f7460a1ead53c255149240cc40911
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-01-15 16:01:43 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1440 ms, Memory: 92636 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		TreeMap<Integer, TreeSet<Integer>> tm = new TreeMap<>();
+		
+		for (int i = 0; i < N; i++) {
+			String[] inputStr = br.readLine().split(" ");
+			int x = Integer.parseInt(inputStr[1]);
+			int y = Integer.parseInt(inputStr[0]);
+			tm.putIfAbsent(x, new TreeSet<>());
+			tm.get(x).add(y);
+		}
+		
+		for (Integer key : tm.keySet()) {
+			TreeSet<Integer> values = tm.get(key);
+			for (Integer value : values) {
+				System.out.println(value + " " + key);
+			}
+		}
+		
+	}
+}

a55d0f0e0e58cd9db62a1605567324895784c046
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-02-04 16:25:11 +0900

    [Silver III] Title: 분수 합, Time: 188 ms, Memory: 18368 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.java
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -18,17 +18,6 @@ public class Main {
 		int b_c = sc.nextInt();
 		int b_p = sc.nextInt();
 		
-		int a_GCD = findGCD(a_c, a_p);
-		if (a_GCD != 1) {
-			a_c /= a_GCD;
-			a_p /= a_GCD;
-		}
-		int b_GCD = findGCD(b_c, b_p);
-		if (b_GCD != 1) {
-			b_c /= b_GCD;
-			b_p /= b_GCD;
-		}
-		
 		int r_p = findLCM(a_p, b_p);
 		int r_c = r_p/a_p*a_c+r_p/b_p*b_c;
 		

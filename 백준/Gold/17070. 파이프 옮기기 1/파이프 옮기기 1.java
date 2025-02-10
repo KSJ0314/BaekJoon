1be644bf8a11fb3db8b29df3c68d7bc057ff5f63
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-02-10 10:13:57 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 288 ms, Memory: 15444 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -9,8 +9,9 @@ public class Main {
 	
 	// ↓↓↓↓↓↓↓↓ 구현 부 ↓↓↓↓↓↓↓↓
 	
+	int n;
 	boolean[][] arr;		// 입력 배열
-	boolean[][][] visited;	// 끝까지 간 pipe의 경우 해당경로는 더 이상 검사하지 않아도 됨.
+	// boolean[][][] visited;	// 끝까지 간 pipe의 경우 해당 경로는 더 이상 검사하지 않아도 됨.
 	int count = 0;			// 파이프를 끝까지 가져갈 수 있는 방법의 수
 	int[][][] dels = {	// 파이프가 움직일 수 있는 방법
 		{	// 가로 파이프의 이동 방법
@@ -31,81 +32,51 @@ public class Main {
 	
 	public void init() {
 		input();
-		Pipe initPipe = new Pipe(0, 1, 0);
-		dfs(initPipe);
+		dfs(0 ,1, 0);
 	}
 	
-	public void dfs(Pipe pipe) {
+	public void dfs(int y, int x, int state) {
 		cnt++;
-		int state = pipe.state;
 		for (int i = 0; i < dels.length; i++) {
 			int[] del = dels[state][i];
 			if (del == null) continue;
 			
-			int ny = pipe.y + del[0];
-			int nx = pipe.x + del[1];
+			int ny = y + del[0];
+			int nx = x + del[1];
 			
 			if (!isIn(ny, nx)) continue;
 			if (arr[ny][nx]) continue;
 			if (i == 2) {
 				if (arr[ny][nx-1] || arr[ny-1][nx]) continue;
 			}
+			
 //			if (i == 2 && (visited[ny][nx][0] || visited[ny][nx][1] || visited[ny][nx][2])
-//					|| visited[ny][nx][i]) {
+//					|| visited[ny][nx][i]) {	// 이미 목표 지점까지 도달 가능했던 경로 체크
 //				count++;
 //				continue;
 //			}
 			
-			int leng = arr.length-1;
-			if (ny == leng && nx == leng) {
+			if (ny == n-1 && nx == n-1) {
 				count++;
-				//visitedSave(pipe.visited);
 				continue;
 			}
 			
-			dfs(new Pipe(ny, nx, i));
-		}
-	}
-	
-	public class Pipe{	// int 5중 배열 사용해도 될 듯? 일단 class가 보기 편함
-		int y;		// 파이프 뒤y
-		int x;		// 파이프 뒤x
-		int state;	// 파이프 상태: 0=가로 | 1=세로 | 2=대각
-		//boolean[][][] visited = new boolean[arr.length][arr.length][3];	// 파이프의 이동 경로 저장: (y, x, state)
-		
-		public Pipe(int y, int x, int state) {
-			this.y = y;
-			this.x = x;
-			this.state = state;
-			//this.visited[y][x][state] = true;
-		}
-	}
-	
-	// 끝까지 간 경로의 경우 값을 저장
-	public void visitedSave(boolean[][][] visited) {
-		for (int i = 0; i < visited.length; i++) {
-			for (int j = 0; j < visited[i].length; j++) {
-				for (int j2 = 0; j2 < visited[i][j].length; j2++) {
-					if (visited[i][j][j2]) {
-						this.visited[i][j][j2] = true;
-					}
-				}
-			}
+			dfs(ny, nx, i);
 		}
 	}
     
 	// 배열 나간거 체크
     public boolean isIn(int y, int x) {
-		return y >= 0 && y < arr.length && x >= 0 && x < arr[0].length;
+		return y >= 0 && y < n && x >= 0 && x < n;
 	}
     
     // 입력과 초기화
     public void input() {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
 			String[] strs;
-			int n = Integer.parseInt(br.readLine());
+			n = Integer.parseInt(br.readLine());
 			arr = new boolean[n][n];
-			visited = new boolean[n][n][3];
+			// visited = new boolean[n][n][3];
 			
 			for (int i = 0; i < n; i++) {
 				strs = br.readLine().split(" ");

a758a4567f749a530fa81917ac4b197ac17d7df6
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-02-23 14:47:22 +0900

    [Bronze V] Title: N 찍기, Time: 260 ms, Memory: 20448 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
-		
+		StringBuilder sb = new StringBuilder();
 		int n = sc.nextInt();
 		
 		for (int i = 1; i <= n; i++) {
-			System.out.println(i);
+			sb.append(i).append("\n");
 		}
+		System.out.println(sb.toString());
 	}
 	
 }

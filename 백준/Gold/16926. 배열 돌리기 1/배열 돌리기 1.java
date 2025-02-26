d49b12c5193d0745c495264b42decc6c21d20a4d
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-02-26 14:19:14 +0900

    [Gold V] Title: 배열 돌리기 1, Time: 1088 ms, Memory: 40988 KB -BaekjoonHub

--- 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -8,32 +8,33 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		init(br);
-		while(R-- >0) {
-			rotate();
+		for (int layer = 0; layer < l; layer++) {
+			int r = R % ((N+M - (4*layer))*2 - 4);
+			while(r-- >0) {
+				rotate(layer);
+			}
 		}
 		print();
 	}
 
-	static void rotate() {
-		for (int layer = 0; layer < l; layer++) {
-			int temp = arr[layer][layer];
-			for (int i = layer+1; i < M-layer; i++) {
-				arr[layer][i-1] = arr[layer][i];
-			}
-			
-			for (int i = layer+1; i < N-layer; i++) {
-				arr[i-1][M-1-layer] = arr[i][M-1-layer];
-			}
-			
-			for (int i = M-1-layer-1; i >= layer; i--) {
-				arr[N-1-layer][i+1] = arr[N-1-layer][i];
-			}
-			
-			for (int i = N-1-layer-1; i > layer; i--) {
-				arr[i+1][layer] = arr[i][layer];
-			}
-			arr[layer+1][layer] = temp;
+	static void rotate(int layer) {
+		int temp = arr[layer][layer];
+		for (int i = layer+1; i < M-layer; i++) {
+			arr[layer][i-1] = arr[layer][i];
 		}
+		
+		for (int i = layer+1; i < N-layer; i++) {
+			arr[i-1][M-1-layer] = arr[i][M-1-layer];
+		}
+		
+		for (int i = M-1-layer-1; i >= layer; i--) {
+			arr[N-1-layer][i+1] = arr[N-1-layer][i];
+		}
+		
+		for (int i = N-1-layer-1; i > layer; i--) {
+			arr[i+1][layer] = arr[i][layer];
+		}
+		arr[layer+1][layer] = temp;
 	}
 
 	static void print() {
@@ -50,7 +51,6 @@ public class Main {
 		N = Integer.parseInt(strs[0]);
 		M = Integer.parseInt(strs[1]);
 		R = Integer.parseInt(strs[2]);
-		//R %= ((N+M)*2)-4;
 		l = Math.min(N/2, M/2);
 		
 		arr = new int[N][M];

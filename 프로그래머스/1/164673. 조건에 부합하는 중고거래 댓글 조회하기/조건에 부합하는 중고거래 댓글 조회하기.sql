3916d7cdbf507c3a84093cb1e5f87891d568d5af
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-03-11 10:31:21 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -2,5 +2,7 @@
 -- 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용, 댓글 작성일
 SELECT  gb.TITLE, gr.BOARD_ID, gr.REPLY_ID, gr.WRITER_ID, gr.CONTENTS, date_format(gr.CREATED_DATE, '%Y-%m-%d') CREATED_DATE
 from    USED_GOODS_BOARD gb join USED_GOODS_REPLY gr on gb.BOARD_ID = gr.BOARD_ID
-where   year(gb.CREATED_DATE) = 2022 && month(gb.CREATED_DATE) = 10
+where   gb.CREATED_DATE between '2022-10-01' and '2022-10-31'
 order by gr.CREATED_DATE, gb.TITLE;
+
+-- where   year(gb.CREATED_DATE) = 2022 && month(gb.CREATED_DATE) = 10

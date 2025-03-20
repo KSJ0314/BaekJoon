25d8214db45e27c646e3538ef0d270a40758fb58
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-03-20 17:51:19 +0900

    [level 3] Title: 징검다리 건너기, Time: 30.20 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,41 +1,6 @@
 class Solution {
-    
-    public static int solution(int[] stones, int k) {
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for (int stone : stones) {
-            min = Math.min(min, stone);
-            max = Math.max(max, stone);
-        }
-
-        while (min < max) {
-            int mid = (min + max + 1) / 2;
-
-            // 조건을 만족하는가
-            if (isPossible(mid, k, stones)) {
-                // 예
-                min = mid;
-            } else {
-                // 아니요
-                max = mid - 1;
-            }
-        }
-        return max;
-    }
-
-    public static boolean isPossible(int num, int k, int[] stones) {
-        int count = 0;
-        for (int stone : stones) {
-            if (stone - num < 0) {
-                count++;
-            } else {
-                count = 0;
-            }
-
-            if (count == k) {
-                return false;
-            }
-        }
-        return true;
+    public int solution(int[] stones, int k) {
+        int answer = 0;
+        return answer;
     }
 }

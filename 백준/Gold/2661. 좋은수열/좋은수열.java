f1dadb8e81d72169919d37e59a421378fbbf05b6
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-03-26 11:56:16 +0900

    [Gold IV] Title: 좋은수열, Time: 124 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.java
+++ 백준/Gold/2661. 좋은수열/좋은수열.java
@@ -1,61 +1,64 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
-
 	static int N;
-	static String ans;
-	static boolean found;
-
-	static void func(int depth, String s) {
-		if(found) {
-			return;
-		}
-		if(!isGood(s)) {
-			return;
-		}
-		
-		if(depth == N) {
-			found = true;
-			ans = new String(s);
-			return; 
-		}
-
-		
-		for(int i=0; i<3; i++) {
-			func(depth+1, s+String.valueOf(i+1));
-		}
-	}
-
-	static boolean isGood(String s) {
-		int k = 1;
-		int i = 0;
-		
-		while (true) {
-			if (i + k >= s.length()) {
-				break;
-			}
-			String back = s.substring(s.length() - k, s.length());
-			String front = s.substring(s.length() - k - k, s.length() - k);
-			//System.out.println(back + " " + front);
-			if(back.equals(front)) {
-				return false;
-			}
-			i++;
-			k++;
-		}
-		return true;
-	}
-
+	static int[] nums;
+	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		N = Integer.parseInt(br.readLine());
-
-		func(1, "1");
-		System.out.println(ans);
+		init();
+		per(0);
+		print();
 	}
 
+	static boolean per(int depth) {
+		if (isBad(depth)) return false;
+		if (depth == N) {
+			return true;
+		}
+		
+		for (int i = 1; i <= 3; i++) {
+			nums[depth]=i;
+			if (per(depth+1)) return true;
+		}
+		
+		return false;
+	}
+
+	static boolean isBad(int depth) {
+		int a,b;
+		for (int i = 1; i <= depth/2; i++) {	// 부분 수열 크기
+			for (int j = 0; j <= depth-(i*2); j++) {	// 부분 수열 시작 index
+				for (int k = j; k <= depth-(i*2); k+=i) {
+					a = numsToInt(k, i-1);
+					b = numsToInt(k+i, i-1);
+					if (a==b) return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	static int numsToInt(int s, int e) {
+		int num = 0;
+		for (int i = s; i <= s+e; i++) {
+			num += nums[i];
+			if (i != s+e) num*=10;
+		}
+		return num;
+	}
+
+	static void print() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			sb.append(nums[i]);
+		}
+		System.out.println(sb);
+	}
+
+	static void init() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		nums = new int[N];
+	}
 }

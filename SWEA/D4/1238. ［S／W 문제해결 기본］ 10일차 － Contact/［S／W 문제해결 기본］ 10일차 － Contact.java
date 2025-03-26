296939c327d702dbbb3cfbf916e21fe58e79aaee
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-03-26 14:16:01 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 86 ms, Memory: 25,216 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
@@ -1,89 +1,63 @@
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
+	static int N, max;
+	static Map<Integer, List<Integer>> map;
+	static Deque<Integer> deque;
+	static Set<Integer> visited;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = 10;
+		for (int test_case = 1; test_case <= T; test_case++) {
+			init(br);
+			bfs();
+			sb.append("#").append(test_case).append(" ").append(max).append("\n");
+		}
+		System.out.println(sb);
+	}
 
-	static int length, start;
-	static int maxVal;
-	static List<Integer>[] lst;
-	static boolean[] visited;
-	static int ans = Integer.MIN_VALUE;
-	static List<Integer> bucket;
-
-	static void bfs(int start) {
-		Queue<Integer> q = new LinkedList<Integer>();
-		q.offer(start);
-		visited[start] = true;
-
-		while (!q.isEmpty()) {
-			bucket = new ArrayList<>();
-			int size = q.size();
-
-			for (int k = 0; k < size; k++) {
-				int cur = q.poll();
-				bucket.add(cur);
-
-				// 인접된 노드수 만큼
-				// 방문하지 않았거나 연결되어있다면 offer
-				for (int i = 0; i < lst[cur].size(); i++) {
-					int val = lst[cur].get(i);
-
-					if (!visited[val]) {
-						visited[val] = true;
-						q.offer(val);
-
-					}
+	static void bfs() {
+		while (!deque.isEmpty()) {
+			int size = deque.size();
+			boolean first = true;
+			while (size-- > 0) {
+				int key = deque.pollFirst();
+				if (visited.contains(key)) continue;
+				visited.add(key);
+				if (first) max = key;
+				max = Math.max(max, key);
+				first = false;
+				if (!map.containsKey(key)) continue;
+				for (int value : map.get(key)) {
+					deque.offerLast(value);
 				}
 			}
 		}
-
 	}
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		for (int tc = 1; tc <= 10; tc++) {
-			String[] s = br.readLine().split(" ");
-
-			ans = Integer.MIN_VALUE;
-			length = Integer.parseInt(s[0]);
-			start = Integer.parseInt(s[1]);
-
-			s = br.readLine().split(" ");
-			for (int i = 0; i < s.length; i++) {
-				maxVal = Math.max(maxVal, Integer.parseInt(s[i]));
-			}
-			lst = new ArrayList[maxVal + 1];
-			for (int i = 0; i < maxVal + 1; i++) {
-				lst[i] = new ArrayList<>();
-			}
-
-			visited = new boolean[maxVal + 1];
-			int idx = 0;
-			for (int i = 0; i < s.length / 2; i++) {
-				int a = Integer.parseInt(s[idx]);
-				int b = Integer.parseInt(s[idx + 1]);
-				// System.out.println(a + " " + b);
-				lst[a].add(b);
-
-				idx += 2;
-			}
-
-			bfs(start);
-			for (int i = 0; i < bucket.size(); i++) {
-				ans = Math.max(ans, bucket.get(i));
-			}
-			System.out.println("#" + tc + " " + ans);
-
-		}
-		}
+	static void init(BufferedReader br) throws IOException {
+		String[] strs = br.readLine().split(" ");
+		N = Integer.parseInt(strs[0]);
+		int start = Integer.parseInt(strs[1]);
+		map = new HashMap<>();
+		deque = new ArrayDeque<>();
+		deque.offerLast(start);
+		max = Integer.MIN_VALUE;
+		visited = new HashSet<>();
 		
+		strs = br.readLine().split(" ");
+		for (int i = 0; i < N; i+=2) {
+			int key =Integer.parseInt(strs[i]);
+			int value =Integer.parseInt(strs[i+1]);
+			mapPut(key, value);
+		}
+	}
 
+	static void mapPut(int key, int value) {
+		if (!map.containsKey(key)) map.put(key, new ArrayList<>());
+		map.get(key).add(value);
+	}
 }

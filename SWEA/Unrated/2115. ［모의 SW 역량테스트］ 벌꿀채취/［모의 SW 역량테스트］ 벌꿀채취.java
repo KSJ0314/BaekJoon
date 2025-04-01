1d59f9815af720e7b20c1923de85dbfae27c34a1
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-04-01 16:18:34 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 89 ms, Memory: 25,344 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
@@ -2,14 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Solution {
 	
 	static int N,M,C;
 	static int[][] arr, honeyScore;
-	static List<Integer> scoreList;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,16 +18,15 @@ public class Solution {
 			
 			init(br);
 			calHoneyScore();
-			selectHoney();
-			Collections.sort(scoreList, Collections.reverseOrder());
 			
-			sb.append(scoreList.get(0));
+			sb.append(selectHoney());
 			sb.append("\n");
 		}
 		System.out.println(sb);
 	}
 
-	static void selectHoney() {
+	static int selectHoney() {
+		int max = Integer.MIN_VALUE;
 		for (int i = 0; i < honeyScore.length; i++) {
 			for (int j = 0; j < honeyScore[i].length; j++) {
 				int a = honeyScore[i][j];
@@ -38,11 +35,12 @@ public class Solution {
 					for (int k2 = j; k2 < honeyScore[k].length; k2++) {
 						if (i==k && j-M < k2 && k2 < j+M) continue;
 						int b = honeyScore[k][k2];
-						scoreList.add(a+b);
+						max = Math.max(max, a+b);
 					}
 				}
 			}
 		}
+		return max;
 	}
 
 	static void calHoneyScore() {
@@ -74,7 +72,6 @@ public class Solution {
 		C = Integer.parseInt(strs[2]);
 		arr = new int[N][N];
 		honeyScore = new int[N][N-M+1];
-		scoreList = new ArrayList<>();
 		
 		for (int i = 0; i < N; i++) {
 			strs = br.readLine().split(" ");

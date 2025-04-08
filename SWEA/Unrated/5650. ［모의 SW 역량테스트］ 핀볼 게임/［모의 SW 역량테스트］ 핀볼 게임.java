2a8807b4e0abd9613480160b4ba09a10b8eb8de1
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-04-08 16:52:02 +0900

    [Unrated] Title: [모의 SW 역량테스트] 핀볼 게임, Time: 1,011 ms, Memory: 47,320 KB -BaekjoonHub

--- SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
+++ SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
@@ -13,7 +13,7 @@ public class Solution {
 		{0, 1}		// 우
 	};
 	
-	static int N;
+	static int N, max;
 	static int[][] map;
 	static Map<Integer, Integer> wormhole;
 	
@@ -27,23 +27,21 @@ public class Solution {
 			init(br);
 			wormholeSetting();
 			
-			int max = 0;
 			for (int i = 1; i <= N; i++) {
 				for (int j = 1; j <= N; j++) {
 					if (map[i][j] != 0) continue;
 					for (int k = 0; k < 4; k++) {
-						max = Math.max(max, pinball(i, j, k));
+						pinball(i, j, k);
 					}
 				}
 			}
 			
-			sb.append(max);
-			sb.append("\n");
+			sb.append(max).append("\n");
 		}
 		System.out.println(sb);
 	}
 
-	static int pinball(int y, int x, int del) {
+	static void pinball(int y, int x, int del) {
 		int ny = y, nx = x, cnt = 0;
 		W: while (true) {
 			do {
@@ -52,37 +50,27 @@ public class Solution {
 				if ((ny == y && nx == x) || map[ny][nx] == -1) break W;
 			} while (map[ny][nx] == 0);
 			
-			if (1 <= map[ny][nx] && map[ny][nx] <= 5) cnt++;
-			
-			switch (map[ny][nx]) {
-				case 1:
-					del = del == 0 ? 1 : del == 1 ? 3 : del == 2 ? 0 : 2;
-					break;
-				case 2:
-					del = del == 0 ? 3 : del == 1 ? 0 : del == 2 ? 1 : 2;
-					break;
-				case 3:
-					del = del == 0 ? 2 : del == 1 ? 0 : del == 2 ? 3 : 1;
-					break;
-				case 4:
-					del = del == 0 ? 1 : del == 1 ? 2 : del == 2 ? 3 : 0;
-					break;
-				case 5:
-					del += del%2==0 ? 1 : -1;
-					break;
-				case 6:
-				case 7:
-				case 8:
-				case 9:
-				case 10:
-					int key = ny*100 + nx;
-					int value = wormhole.get(key);
-					ny = value/100;
-					nx = value%100;
-					break;
+			if (1 <= map[ny][nx] && map[ny][nx] <= 5) {
+				del = changeDel(map[ny][nx], del);
+				cnt++;
+			} else {
+				int key = ny*100 + nx;
+				int value = wormhole.get(key);
+				ny = value/100;
+				nx = value%100;
 			}
 		}
-		return cnt;
+		max = Math.max(max, cnt);
+	}
+
+	static int changeDel(int num, int del) {
+		switch (num) {
+			case 1 : return del == 0 ? 1 : del == 1 ? 3 : del == 2 ? 0 : 2;
+			case 2 : return del == 0 ? 3 : del == 1 ? 0 : del == 2 ? 1 : 2;
+			case 3 : return del == 0 ? 2 : del == 1 ? 0 : del == 2 ? 3 : 1;
+			case 4 : return del == 0 ? 1 : del == 1 ? 2 : del == 2 ? 3 : 0;
+			default : return (del + (del%2==0 ? 1 : -1));
+		}
 	}
 
 	// 같은 번호의 웜홀을 매핑
@@ -113,8 +101,8 @@ public class Solution {
 		N = Integer.parseInt(br.readLine().trim());
 		map = new int[N+2][N+2];
 		wormhole = new HashMap<>();
+		max = Integer.MIN_VALUE;
 		
-		Arrays.fill(map[0], 5);
 		for (int i = 1; i <= N; i++) {
 			map[i][0] = map[i][N+1] = 5;
 			strs = br.readLine().split(" ");
@@ -122,6 +110,7 @@ public class Solution {
 				map[i][j] = Integer.parseInt(strs[j-1]);
 			}
 		}
+		Arrays.fill(map[0], 5);
 		Arrays.fill(map[N+1], 5);
 	}
 }

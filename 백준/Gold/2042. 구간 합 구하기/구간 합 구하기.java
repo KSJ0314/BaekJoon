151fc90ff89f67ee68801fa62fa559b1a40d32ad
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-04-16 00:03:27 +0900

    [Gold I] Title: 구간 합 구하기, Time: 552 ms, Memory: 120252 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
 	static int N, MK;
@@ -8,68 +7,70 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		String[] strs;
 		init(br);
-		initTree(0,N-1,1);
 		
-		while (MK-- >0) {
-			String[] strs = br.readLine().split(" ");
-			int flag = Integer.parseInt(strs[0]);
-			int a = Integer.parseInt(strs[1]);
-			long b = Long.parseLong(strs[2]);
+		makeTree(0,N-1,1);
+		
+		while (MK-- > 0) {
+			strs = br.readLine().split(" ");
+			int oper = Integer.parseInt(strs[0]);
 			
-			if (flag == 1) {
-				int idx = a-1;
-				long diff = b - arr[idx];
+			if (oper == 1) {
+				int idx = Integer.parseInt(strs[1])-1;
+				long value = Long.parseLong(strs[2]);
+				long diff = value - arr[idx];
 				updateTree(0, N-1, 1, idx, diff);
-				arr[idx] = b;
+				arr[idx] = value;
 			} else {
-				int l = a-1;
-				int r = (int) (b-1);
+				int l = Integer.parseInt(strs[1])-1;
+				int r = Integer.parseInt(strs[2])-1;
 				long sum = sumTree(0, N-1, 1, l, r);
 				sb.append(sum).append("\n");
 			}
 		}
-		System.out.println(sb.toString());
+		
+		System.out.println(sb);
 	}
 
-	private static long initTree(int s, int e, int node) {
-		if (s == e) return tree[node] = arr[s];
+	static long makeTree(int s, int e, int n) {
+		if (s == e) return tree[n] = arr[s];
 		int mid = (s+e)/2;
-		return tree[node] = initTree(s, mid, node*2) + initTree(mid+1, e, node*2+1);
-	}
-
-	private static long sumTree(int s, int e, int node, int l, int r) {
-		if (r < s || e < l) return 0;
-		if (l <= s && e <= r) return tree[node];
-		
-		int mid = (s+e)/2;
-		
-		return sumTree(s, mid, node*2, l, r) + sumTree(mid+1, e, node*2+1, l, r);
+		return tree[n] = makeTree(s, mid, n*2) + makeTree(mid+1, e, n*2+1);
 	}
 	
-	private static void updateTree(int s, int e, int node, int idx, long diff) {
-		if (s <= idx && idx <= e) {
-			tree[node] += diff;
-		} else return;
+	static long sumTree(int s, int e, int n, int l, int r) {
+		if (r < s || e < l) return 0;
+		if (l <= s && e <= r) return tree[n];
+		int mid = (s+e)/2;
+		return sumTree(s, mid, n*2, l, r) + sumTree(mid+1, e, n*2+1, l, r);
+	}
+	
+	static void updateTree(int s, int e, int n, int idx, long diff) {
+		if (idx < s || e < idx) return;
+		
+		tree[n] += diff;
 		
 		if (s == e) return;
 		
 		int mid = (s+e)/2;
-		updateTree(s, mid, node*2, idx, diff);
-		updateTree(mid+1, e, node*2+1, idx, diff);
+		updateTree(s, mid, n*2, idx, diff);
+		updateTree(mid+1, e, n*2+1, idx, diff);
 	}
 
-	public static void init(BufferedReader br) throws IOException {
+	static void init(BufferedReader br) throws IOException {
 		String[] strs = br.readLine().split(" ");
 		N = Integer.parseInt(strs[0]);
 		MK = Integer.parseInt(strs[1]);
 		MK += Integer.parseInt(strs[2]);
+		
 		arr = new long[N];
-		tree = new long[4*N+1];
+		tree = new long[4*N];
 		
 		for (int i = 0; i < N; i++) {
 			arr[i] = Long.parseLong(br.readLine());
 		}
+		
 	}
 	
 }

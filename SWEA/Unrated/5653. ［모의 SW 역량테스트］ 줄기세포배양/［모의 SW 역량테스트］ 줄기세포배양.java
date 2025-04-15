bbd5cddabc52576633a3260c25de22b289127b9a
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-04-15 13:54:01 +0900

    [Unrated] Title: [모의 SW 역량테스트] 줄기세포배양, Time: 427 ms, Memory: 101,520 KB -BaekjoonHub

--- SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.java
+++ SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.java
@@ -20,10 +20,10 @@ public class Solution {
 	}
 	static int[][] dels = {{-1,0},{1,0},{0,-1},{0,1}};
 	static int N, M, K;
-	static List<Cell> removeCells;
-	static PriorityQueue<Cell> addCells;
-	static int[][] arr;
-	static Set<Cell> cells;
+	static Set<Integer> keys;				// 세포가 저장된 좌표 저장 (좌표를 key로 매핑해서 이미 사용중인 key인지 확인)
+	static Set<Cell> cells;					// 전체 순회, 추가, 특정 객체 제거 잦음 : HashSet
+	static PriorityQueue<Cell> addCells;	// 정렬해서 하나씩 다 빼기 : PQ
+	static List<Cell> removeCells;			// 추가, 전체 순회만 : ArrayList
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -48,24 +48,26 @@ public class Solution {
 	static void fnc() {
 		while (K-- > 0) {
 			removeCells.clear();
+			
 			for (Cell cell : cells) {
 				cell.time--;
-				if (cell.time == -1) {	// 세포 분열
+				if (cell.time == -1) {	// 분열할 세포 저장
 					addCells.add(cell);
 				}
-				if (cell.time == cell.life*-1) {
+				if (cell.time == cell.life*-1) {	// 사망할 세포 저장 (forEach 돌면서 remove 하면 안됨)
 					removeCells.add(cell);
 				}
 			}
 			
-			while (!addCells.isEmpty()) {
+			while (!addCells.isEmpty()) {	// 세포 분열(생명력이 큰 세포부터)
 				Cell cell = addCells.poll();
 				for (int[] del : dels) {
 					int ny = cell.y + del[0];
 					int nx = cell.x + del[1];
+					int key = ny*1000 + nx;
 					
-					if (arr[ny][nx] == 0) {
-						arr[ny][nx] = cell.life;
+					if (!keys.contains(key)) {	// 분열 성공
+						keys.add(key);
 						cells.add(new Cell(ny, nx, cell.life));
 					}
 				}
@@ -83,10 +85,10 @@ public class Solution {
 		String[] strs;
 		strs =  br.readLine().split(" ");
 		
+		keys = new HashSet<>();
 		cells = new HashSet<>();
 		addCells = new PriorityQueue<>();
 		removeCells = new ArrayList<>();
-		arr = new int[500][500];
 		
 		N = Integer.parseInt(strs[0]);
 		M = Integer.parseInt(strs[1]);
@@ -98,7 +100,8 @@ public class Solution {
 				int num = Integer.parseInt(strs[j]);
 				if (num != 0) {
 					cells.add(new Cell(200+i, 200+j, num));
-					arr[200+i][200+j] = num;
+					int key = (i+200)*1000 + (j+200);
+					keys.add(key);
 				}
 			}
 		}

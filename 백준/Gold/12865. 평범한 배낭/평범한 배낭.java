3dc4c50d3ca11154c8d895a8d4f36c3cf3e09dfe
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-11-15 21:28:10 +0900

    [Gold V] Title: 평범한 배낭, Time: 128 ms, Memory: 14592 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,12 +1,13 @@
 import java.io.*;
-import java.util.*;
 
+// 2차원 배열 사용과 비교했을 때 메모리 차이 큼
+// 53792KB / 14612KB (1차원)
 public class Main {
 	static class Thing{		// 가독성을 위해 물건을 class로 저장
-		int weight, value;
-		public Thing(int weight, int value) {
-			this.weight = weight;
-			this.value = value;
+		int w, v;
+		public Thing(int w, int v) {
+			this.w = w;
+			this.v = v;
 		}
 	}
 	
@@ -16,21 +17,16 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		init();
-		
 		makeDp();
-		
 		System.out.println(dp[K]);
 	}
 	
 	public static void makeDp() {
-		for (int i = 1; i <= N; i++) {
-			int w = things[i].weight;
-			int v = things[i].value;
-			
+		for (Thing thing : things) {
 			// 중복 저장이 안되므로 뒤에서부터 저장
 			// w 이상만 체크
-			for (int j = K; j >= w; j--) {
-				dp[j] = Math.max(dp[j], dp[j-w] + v);
+			for (int j = K; j >= thing.w; j--) {
+				dp[j] = Math.max(dp[j], dp[j-thing.w] + thing.v);
 			}
 		}
 	}
@@ -41,14 +37,12 @@ public class Main {
 		N = Integer.parseInt(strs[0]);
 		K = Integer.parseInt(strs[1]);
 		
-		things = new Thing[N+1];
+		things = new Thing[N];
 		dp = new int[K+1];
 		
-		for (int i = 1; i <= N; i++) {
+		for (int i = 0; i < N; i++) {
 			strs = br.readLine().split(" ");
-			int weight = Integer.parseInt(strs[0]);
-			int value = Integer.parseInt(strs[1]);
-			things[i] = new Thing(weight, value);
+			things[i] = new Thing(Integer.parseInt(strs[0]), Integer.parseInt(strs[1]));
 		}
 	}
 	

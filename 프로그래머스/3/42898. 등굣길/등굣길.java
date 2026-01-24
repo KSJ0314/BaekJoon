27e7eac40ecd5604757f8b36b7a3c750d911f244
ksj <132119447+KSJ0314@users.noreply.github.com>
2026-01-25 02:16:14 +0900

    [level 3] Title: 등굣길, Time: 0.79 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+    	final int MOD = 1_000_000_007;
+    	int[][] dp = new int[m+1][n+1];
+    	boolean[][] isPuddles = new boolean[m+1][n+1];
+    	for (int[] puddle : puddles) {
+    		isPuddles[puddle[0]][puddle[1]] = true;
+    	}
+    	dp[0][1] = 1;
+    	for (int i = 1; i <= m; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (isPuddles[i][j]) continue;
+				dp[i][j] = dp[i-1][j] + dp[i][j-1];
+				dp[i][j] %= MOD;
+			}
+		}
+    	return dp[m][n];
+    }
+}

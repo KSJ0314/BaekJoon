2b1181bda57e7056ceeeca45629ff0cf1c25bd7e
ksj <132119447+KSJ0314@users.noreply.github.com>
2026-04-22 10:49:07 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 148 ms, Memory: 21492 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -2,61 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int S, E;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] strs;
 		
+		final int LIMIT = 100001;
+		
 		strs = br.readLine().split(" ");
-		S = Integer.parseInt(strs[0]);
-		E = Integer.parseInt(strs[1]);
+		int N = Integer.parseInt(strs[0]);
+		int K = Integer.parseInt(strs[1]);
 		
-		if (S == E) {
-			System.out.println(0);
-			System.out.println(1);
-			return;
+		int[] depth = new int[LIMIT];
+		int[] cnt = new int[LIMIT];
+		
+		Arrays.fill(depth, LIMIT);
+		
+		depth[N] = 0;
+		cnt[N] = 1;
+		
+		ArrayDeque<Integer> deq = new ArrayDeque<>();
+		deq.add(N);
+		
+		while (!deq.isEmpty()) {
+			int crt = deq.pollFirst();
+			if (depth[crt] > depth[K]) break;
+			
+			int[] nexts = {crt-1, crt+1, crt*2};
+			
+			for (int next : nexts) {
+				if (next < 0 || next >= LIMIT) continue;
+				
+				if (depth[next] == LIMIT) {
+					depth[next] = depth[crt] + 1;
+                    cnt[next] = cnt[crt];
+                    deq.addLast(next);
+				} else if (depth[next] == depth[crt] + 1) {
+					cnt[next] += cnt[crt];
+	            }
+			} 
 		}
 		
-		ArrayDeque<Integer> idxs = new ArrayDeque<>();
-		idxs.add(S);
-		int time = 0;
-		boolean isEnd = false;
-		int cnt = 0;
-		boolean[] visited = new boolean[100001];
-		visited[S] = true;
-		while (!idxs.isEmpty()) {
-			int size = idxs.size();
-			time++;
-			HashSet<Integer> visitedSet = new HashSet<>();
-			
-			while (size-- > 0) {
-				int crt = idxs.pollFirst();
-				
-				int[] nexts = {crt-1, crt+1, crt*2};
-				
-				for (int next : nexts) {
-					if (next == E) {
-						isEnd = true;
-						cnt++;
-					}
-					if (0 <= next && next <= 100000) {
-						if (visited[next]) continue;
-						visitedSet.add(next);
-						idxs.addLast(next);
-					}
-				}
-			}
-			
-			for (int v : visitedSet) {
-				visited[v] = true;
-			}
-			
-			if (isEnd) {
-				System.out.println(time);
-				System.out.println(cnt);
-				break;
-			}
-		}
+		System.out.println(depth[K]);
+        System.out.println(cnt[K]);
 	}
 }

ab4cc22c58cf90c52e9f8e19d3ee31b0773ba389
ksj <132119447+KSJ0314@users.noreply.github.com>
2026-04-28 11:12:23 +0900

    [Gold III] Title: 세 용액, Time: 228 ms, Memory: 17272 KB -BaekjoonHub

---
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] arr = new long[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        long min = Long.MAX_VALUE;
+        long a = 0, b = 0, c = 0;
+
+        for (int i = 0; i < N - 2; i++) {
+            int left = i + 1;
+            int right = N - 1;
+
+            while (left < right) {
+                long sum = arr[i] + arr[left] + arr[right];
+
+                if (Math.abs(sum) < min) {
+                    min = Math.abs(sum);
+                    a = arr[i];
+                    b = arr[left];
+                    c = arr[right];
+                }
+
+                if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+
+        System.out.println(a + " " + b + " " + c);
+    }
+}
